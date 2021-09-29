b0982c1c469c8af0fba22e6e2b647ba85c06bfdd
Abinesh <2001mrabi@gmail.com>
2021-09-29 20:43:35 +0530

    main

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Todolist from "./components/Todolist";
+import "./App.css";
+
+const App = () => {
+  
+  return (
+    <div>
+      <Todolist />
+    </div>
+  );
+};
+
+export default App;
